bb8032695e531e6ce6f8702300b6d3711ec35989
Justin Garrison <justinleegarrison@gmail.com>
2014-12-08 21:36:58 -0800

    added more samples

--- social_spoilers.js
+++ social_spoilers.js
@@ -8,4 +8,21 @@
 
 // Regex to match what I need /\^[a-zA-Z0-9:.,?! ]+[^#|^|@|\$]/g
 
-// A sample text to match <p class="js-tweet-text tweet-text">RT <a href="/NetAppGeek" class="twitter-atreply pretty-link" dir="ltr" ><s>@</s><b>NetAppGeek</b></a> I enjoy ^filenames with the dotted circle ( ? ) the interrobang ( ? ) and^ my fave: irony punctuation ( ? ) &lt; This = The worst!</p>
+// Samples to match 
+// <p class="js-tweet-text tweet-text">RT <a href="/NetAppGeek" class="twitter-atreply pretty-link" dir="ltr" >
+// <s>@</s>
+// <b>NetAppGeek</b></a> I enjoy ^filenames with the dotted circle ( ? ) the interrobang ( ? ) and^ my fave: irony punctuation ( ? ) &lt; This = The worst!</p>
+//
+// <p class="js-tweet-text tweet-text">RT <a href="/NetAppGeek" class="twitter-atreply pretty-link" dir="ltr" >
+// This is a test that should hide ^To the end of the line.
+//
+// <p class="js-tweet-text tweet-text">RT <a href="/NetAppGeek" class="twitter-atreply pretty-link" dir="ltr" >
+// Begining of the line^ spoiler match!
+// 
+// Snape ^killed dumbledore^
+//
+// Darth Vader ^is Luke's father^!
+//
+// Can you believe ^Bruce Wilis was dead the whole time?!
+//
+// ^TYLER DURDEN DOESN’T EXIST^
