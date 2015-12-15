bf64f981ad0be73bccd87648e1f6f26637d20367
Kyle Hayes <mrkylehayes@gmail.com>
2015-12-15 12:29:47 -0800

    Adding support for blacklist saving to chrome storage

---
+++ popup.js
@@ -0,0 +1,11 @@
+$(function(){
+
+  // save blacklist click handler
+  $('#saveBlacklist').click(function(event){
+    var blacklist = $('#blacklist').val().split(/\n/);
+    console.log(blacklist);
+    chrome.storage.sync.set({'blacklist': blacklist}, function() {
+      console.log('blacklist saved')
+    })
+  })
+});
