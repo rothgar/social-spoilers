ecda5a15badef33b656e9f7d4dbd3a72947a3510
Justin Garrison <justinleegarrison@gmail.com>
2015-12-15 23:32:22 -0800

    renamed blacklist to userBlacklist for clarity

--- popup.js
+++ popup.js
@@ -2,9 +2,12 @@ $(function(){
 
   // save blacklist click handler
   $('#saveBlacklist').click(function(event){
-    var blacklist = $('#blacklist').val().split(/\n/);
+    var blacklist = $('#userBlacklist').val().split(/\n/);
     console.log(blacklist);
-    chrome.storage.sync.set({'blacklist': blacklist}, function() {
+    chrome.storage.sync.set({'list': blacklist}, function() {
+      if (chrome.runtime.error) {
+        console.log("Error saving blacklist");
+      }
       console.log('blacklist saved');
       window.close();
     });
