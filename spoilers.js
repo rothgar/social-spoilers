7d985fe30329368920f4e6c85d19659e803380d8
Justin Garrison <justinleegarrison@gmail.com>
2015-12-17 08:21:51 -0800

    trying to make match hashtag loop

--- spoilers.js
+++ spoilers.js
@@ -8,13 +8,24 @@
 
 // Regex to match what I need /\^[a-zA-Z0-9:.,?! ]+[^#|^|@|\$]/g
 
-var blacklist = ["StarWars","#TTY","foo"];
+var blacklist = ["#StarWars","#TTY","foo"];
 
 function loadBlacklist() {
   chrome.storage.sync.get('blacklist', function(list){
     blacklist = list.blacklist;
   });
 }
+
+function matchHashtag(hashtag) {
+  var newText = text.replace(/.*/, ['<span class="spoiler">', text, '</span>'].join(''));
+  var $newText = $(newText);
+  var hashtags = $newText.find('.twitter-hashflag-container');
+  hashtags.each(function(idx, hashtag, arr) {
+    var outerhtml = hashtag.outerHTML;
+    var newnewText = newText.replace(outerhtml, ['</span>', outerhtml, '<span class="spoiler">'].join(''));
+    $content.html(newnewText);
+  });
+};
 
 // loadBlacklist();
 
@@ -24,9 +35,9 @@ a = $('.stream-container li[id|="stream-item-tweet"]');
 // read tweet content
 a.each(function() {
   var $content = $(this).find('div > div.content > p');
-  var text = $content.html();
-  if(text.match(/\^.+\^/)) {
-    var newText = text.replace(/\^.+\^/g, ['<span class="spoiler">', text.match(/\^.+\^/), '</span>'].join(''))
+  var regexText = $content.text;
+  if(regexText.match(/\^.+\^/)) {
+    var newText = regexText.replace(/\^.+\^/g, ['<span class="spoiler">', regexText.match(/\^.+\^/), '</span>'].join(''))
 
     // target this class if you want to do something with the whole tweet
     $content.addClass('tweet-spoiler')
@@ -34,15 +45,15 @@ a.each(function() {
   };
   // loop blacklist
   blacklist.forEach(function(regex) {
-    if(text.toLowerCase().match(regex.toLowerCase())) {
-      var newText = text.replace(/.*/, ['<span class="spoiler">', text, '</span>'].join(''));
-      var $newText = $(newText);
-      var hashtags = $newText.find('.twitter-hashflag-container');
-      hashtags.each(function(idx, hashtag, arr) {
-        var outerhtml = hashtag.outerHTML;
-        var newnewText = newText.replace(outerhtml, ['</span>', outerhtml, '<span class="spoiler">'].join(''));
-        $content.html(newnewText);
-      });
+      // need special selection for hashtags
+      if(regex.match(/^#.+/)) {
+        var text = $content.html();
+        matchHashtag(regex);
+      } else {
+        var text = $content.text;
+        console.log(regex);
+        if(text.toLowerCase().match(regex.toLowerCase())) {
+      };
 
       $content.addClass('tweet-spoiler')
     };
